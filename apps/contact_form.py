d3ddcab2597aa116a791d71da57b4b1447d16fa0
Harsh Singh <62803431+Harshstag@users.noreply.github.com>
2022-07-18 22:30:44 +0530

    Update contact_form.py

--- apps/contact_form.py
+++ apps/contact_form.py
@@ -1,4 +1,5 @@
 import streamlit as st  # pip install streamlit
+import webbrowser
 
 def app():
     # st.header(":mailbox: Get In Touch With Me!")
@@ -28,3 +29,24 @@ def app():
 
 
     local_css("style/style.css")
+    
+    
+    
+#     *******************************************************************************************************************************************************
+
+
+
+
+    url = 'https://form.jotform.com/221963166587467'
+
+    # st.write("<h style=' color: #0078ff; font-size:50px;'>Send Feedback</h>", unsafe_allow_html=True)    
+    st.write("<h style='font-family:Montserrat; font-weight: bold; font-size:50px;'>SEND FEEDBACK</h>", unsafe_allow_html=True)
+    st.markdown("<hr/>", unsafe_allow_html=True)
+
+
+    st.write("<h style='  font-size:30px;'>Your feedback is highly appreciated and will help us to improve our ability to serve you and other users of our web sites. For your convenience, we provide you the following options:</h>", unsafe_allow_html=True)
+
+    if st.button('Click Here to send Feedback'):
+        webbrowser.open_new_tab(url)   
+
+  
